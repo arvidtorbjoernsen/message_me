02d8f24a7477b9bd846a117ad47420db702c6b03
arvidtorbjoernsen <atorbj@gmail.com>
2020-05-12 19:45:55 +0200

    Added 1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'chatroom#index'
+  get 'login', to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
