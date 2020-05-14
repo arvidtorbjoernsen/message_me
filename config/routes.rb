68b430750fede427a0560b57cf67dd1168268ffb
arvidtorbjoernsen <atorbj@gmail.com>
2020-05-14 12:00:42 +0200

    Over til laptop

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@
 
 Rails.application.routes.draw do
   root 'chatroom#index'
-  # get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
 end
